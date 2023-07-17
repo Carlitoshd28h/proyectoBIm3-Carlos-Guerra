48250b8483a4ff28abb604d97970bd94cd70b84b
unknown <correo>
2023-07-17 12:36:48 -0600

    holap

--- Agendatelefonica1/src/agendatelefonica1/Contacto1.java
+++ Agendatelefonica1/src/agendatelefonica1/Contacto1.java
@@ -1,5 +1,35 @@
 package agendatelefonica1;
 
 public class Contacto1 {
-    
+     String telefono, nombre, domicilio;
+
+    public Contacto1 (String telefono, String nombre, String domicilio) {
+        this.telefono = telefono;
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+    }
+
+    public String verTelefono() {
+        return telefono;
+    }
+
+    public String verNombre() {
+        return nombre;
+    }
+
+    public String verDomicilio() {
+      return domicilio;
+    }
+  public void setTelefono(String telefono){
+      this.telefono = telefono;
+  }        
+
+  public void setNombre(String nombre){
+      this.nombre = nombre;
+  }
+  public void setDomicilio(String domicilio){
+      this.domicilio = domicilio;
+      
+  }
+  
 }
